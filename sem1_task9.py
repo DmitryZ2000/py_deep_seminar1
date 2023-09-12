464a3e6f9c7542b28e4cedec715d8f326b5a01bb
dmitry_zaykin <orionvega5@list.ru>
2023-09-12 22:13:15 +0300

    'Update'

--- sem1_task9.py
+++ sem1_task9.py
@@ -1,12 +1,12 @@
 #Выведите в консоль таблицу умножения от 2х2 до 9х10 как на школьной тетрадке
-for i in range(2,11):
-    for j in range(2,6):
+for i in range(2,11): # Множитель
+    for j in range(2,6): # Умножаемое
         if j * i < 10:
-            print(f'{j} x {i} = {j * i}', sep=' ', end='     ' )
+            print(f'{j} x {i} = {j * i}', end='     ' ) # Результат меньше 10
         elif j * i >=10 and i != 10:
-            print(f'{j} x {i} = {j * i}', sep=' ', end='    ' )
+            print(f'{j} x {i} = {j * i}', end='    ' ) # Результат двузачный
         else:
-            print(f'{j} x {i} ={j * i}', sep=' ', end='    ' )
+            print(f'{j} x {i} ={j * i}', end='    ' ) # Умножаем на 10
     print()
 
 print()
@@ -14,9 +14,9 @@ print()
 for i in range(2,11):
     for j in range(6,10):
         if j * i < 10:
-            print(f'{j} x {i} = {j * i}', sep=' ', end='     ' )
+            print(f'{j} x {i} = {j * i}',  end='     ' )
         elif j * i >=10 and i != 10:
-            print(f'{j} x {i} = {j * i}', sep=' ', end='    ' )
+            print(f'{j} x {i} = {j * i}',  end='    ' )
         else:
-            print(f'{j} x {i} ={j * i}', sep=' ', end='    ' )
+            print(f'{j} x {i} ={j * i}', end='    ' )
     print()
